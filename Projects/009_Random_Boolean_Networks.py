0b67ec3d68acddb4f328267259ccc5c58ec8a7db
Guilherme Silva <guilhermebaos@gmail.com>
2020-11-18 21:50:23 +0000

    Upgrade do the Random Boolean Networks script
    + Added Perturbations
    + Changed how the result was displayed

--- Projects/009_Random_Boolean_Networks.py
+++ Projects/009_Random_Boolean_Networks.py
@@ -18,12 +18,20 @@ def join_list(lst, string=''):
     return lst
 
 
+states = [0, 1]         # States
+
 # Select parameters
 nodes_num = 20          # Number of Nodes
 connections_num = 3     # Connections per Node
-steps = 20              # Number os Steps computed
+steps = 30              # Number os Steps computed
 show_colors = True      # Show colors instead of 0s and spaces
-states = [0, 1]
+on_color = 'green'      # Color of the 'On' Nodes
+
+# The step in which the bits are flipped will be indicated by a blue number,
+# The first perturbation_num bits will be flipped
+perturbation = True     # Cause perturbations? (bit flips)
+perturbation_step = 15  # When will the perturbations occur
+perturbation_num = 5    # How many bits will be flipped
 
 # Can Nodes receive more than 1 bit from one other Node, e.g:
 # if True, than Node 1 can have the following connections [2, 2, 3]
@@ -33,6 +41,9 @@ repeat_connections = True
 
 if show_colors:
     from termcolor import cprint
+
+if perturbation_num >= nodes_num:
+    raise Exception('The number of perturbed nodes must be lower than the total number of nodes')
 
 # Create the first nodes and their connections
 nodes = []
@@ -52,7 +63,9 @@ truth_table = []
 for _ in range(2 ** connections_num):
     truth_table += [choice(states)]
 
-for _ in range(steps):
+steps_len = len(str(steps))
+background_color = 'on_' + on_color
+for current_step in range(steps):
     # Calculate the states of the new nodes
     new_nodes = []
     for node_list in connections:
@@ -61,27 +74,32 @@ for _ in range(steps):
             feed += str(nodes[node_index])
         new_nodes += [truth_table[binaryToDecimal(feed)]]
 
+    # Create the perturbations (bit flips)
+    if perturbation and current_step == perturbation_step:
+        cprint(str(current_step).rjust(steps_len), 'blue', end=' ')
+        for i in range(perturbation_num):
+            new_nodes[i] = (new_nodes[i] + 1) % 2
+    else:
+        print(str(current_step).rjust(steps_len), end=' ')
+
     # Print the results
     for boolean in new_nodes[:-1]:
         if show_colors:
             if boolean == 0:
-                color = 'red'
-                on_color = 'on_red'
+                print(' ', end='')
             else:
-                color = 'green'
-                on_color = 'on_green'
-            cprint(' ', color, on_color, end='')
+                cprint(' ', on_color, background_color, end='')
         else:
             if boolean == 0:
-                text = '0'
-            else:
                 text = ' '
+            else:
+                text = '1'
             print(text, end='')
     if show_colors:
         if new_nodes[-1] == 0:
-            cprint(' ', 'red', 'on_red')
+            print(' ')
         else:
-            cprint(' ', 'green', 'on_green')
+            cprint(' ', on_color, background_color)
     else:
         if new_nodes[-1] == 0:
             print('0')
